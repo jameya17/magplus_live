b30469414b756b30e7317ac5d5adfe07aab8cae7
amjadhav <amjadhav@gracenote.com>
2020-04-03 18:13:39 +0530

    Files modified

--- wp-content/themes/twentynineteen/footer.php
+++ wp-content/themes/twentynineteen/footer.php
@@ -63,6 +63,7 @@
             </div>
 
             <h6>Contact: <a href="tel:+91 80876 00106" title="tel:+91 80876 00106">+91 80876 00106</a></h6>
+            <?php echo do_shortcode('[do_widget id=wp_subscribe-3]'); ?>
         </div>
         <div class="mob-view">
             <div class="follow-us-block">
