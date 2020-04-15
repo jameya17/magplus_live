2b2a63084ea93dc22dc18b8d1a70351af0054b77
Arjun Sonawane <arjunsonawane@Arjuns-MacBook-Air.local>
2020-04-15 20:13:55 +0530

    Committing download page template

---
+++ wp-content/themes/twentynineteen/templates/download.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template Name: Download New Template
+ *
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+    <div class="container download-pg">
+        <section  class="l-section sec-pad">
+            <div class="l-section-wrap">
+                <h3>Download the<br/> mag+ Creative Tools</h3>
+                <div class="g-col">
+                    <div class="one-half">
+                        <div class="drop-down">
+                            <div class="selected">
+                                <a href="javascript:void"> CC 2020</span></a>
+                            </div>
+                            <div class="options">
+                                <ul>
+                                    <li><a href="javascript:void" id="val1"> CC 2020</a></li>
+                                    <li><a href="javascript:void" id="val2" > CC 2019</a></li>
+                                    <li><a href="javascript:void" id="val3"> CC 2018</a></li>
+                                </ul>
+                            </div>    
+                        </div>  
+
+                        <p>Shown above is the latest InDesign version. To select a different version, choose from the drop-down menu.</p>
+                        
+                        <div class="radio-button-block">
+                            <label class="checkbox-container" for="">
+                                <input type="radio">
+                                <span class="checkmark"></span> Mac OS X
+                            </label>
+                            <label class="checkbox-container" for="">
+                                <input type="radio">
+                                <span class="checkmark"></span> Windows
+                            </label>
+                        </div>
+
+                        <div class="btn-block">
+                            <a href="" class="primary-btn" title="Download">Download
+                                <span class="span1">Download</span>
+                                <span class="span2">Download</span>
+                                <span class="span3">Download</span>
+                            </a>
+                        </div>
+                        
+                        <p class="tmc-text">By downloading I am agreeing to the <a href="?page_id=31545" title="mag+ Designd Terms & Conditions">mag+ Designd Terms & Conditions</a></p>
+                    </div>
+                    <div class="one-half">
+                        <div class="tools-download-info-holder">
+                            <div class="tools-download-info-block">
+                                <h5>Your Tools download will contain:</h5>
+                                <ul>
+                                    <li>InDesign templates for all supported devices</li>
+                                    <li>The mag+ InDesign Plugin to create your interactive documents</li>
+                                    <li>The Production Tool to assemble your pages and create your mag+ file</li>
+                                </ul>
+                            </div>
+                            <h6 class="text-link">Get Started Guide</h6>
+                            <p>See the latest release notes for the <a href="#" class="text-link" title="mag+ Creative Tools">mag+ Creative Tools</a>.</p>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </section> 
+    </div>
+	
+
+<?php
+get_footer();
