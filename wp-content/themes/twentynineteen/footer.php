8265fed0a86f8a73165349222d2b5911888e19c7
Sandesh Kardekar <sandyinsan@gmail.com>
2020-06-11 19:04:15 +0530

    style chnages

--- wp-content/themes/twentynineteen/footer.php
+++ wp-content/themes/twentynineteen/footer.php
@@ -361,7 +361,7 @@
 
 
                 // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-                offset: 300, // offset (in px) from the original trigger point    
+                offset: 200, // offset (in px) from the original trigger point    
                 delay: 0, // values from 0 to 3000, with step 50ms
                 duration: 600, // values from 0 to 3000, with step 50ms  
                 easing: 'ease', // default easing for AOS animations
