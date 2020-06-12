5cee4528f868254eb814bd4ee97acb1162c195ba
amjadhav <amjadhav@gracenote.com>
2020-06-12 19:55:49 +0530

    File modified

--- wp-content/themes/twentynineteen/templates/download.php
+++ wp-content/themes/twentynineteen/templates/download.php
@@ -8,6 +8,38 @@
  * @since 1.0.0
  */
 
+//Set version of download to be passed to google analytics for tracking.
+
+$path = get_stylesheet_directory();
+require_once($path.'/logged-in/functions.php');
+
+
+if(!isset($_COOKIE['mg_version'])){
+
+    $mpUpdate='./magplus/mpidupdate.txt';
+
+    if(file_exists($mpUpdate)){
+        $version = file_get_contents($mpUpdate);
+
+        $versionSlicer = explode(substr($version, 1, 1), $version);
+
+        $versiontrk = $versionSlicer[0].'.'.$versionSlicer[1].'.'.$versionSlicer[2];
+
+        setcookie('mg_version', $versiontrk, time()+3600, "/");/* Passes the mag+ version to my-magplus for tracking when downloadinf */
+    }
+
+}
+
+
+$nowFormat = date('Y-m-d'); $timeFormat = date('H:i:s'); 
+$ipaddress = $_SERVER['REMOTE_ADDR']; 
+$data = magplus_logged_in_check();
+
+if (empty($data)) { 
+    //header("location:https://www.magplus.com/signup/");
+    //exit;
+} 
+
 get_header();
 ?>
     <div class="container downloal-page">
@@ -16,6 +48,48 @@ get_header();
                 <h3>Download the<br/> mag+ Creative Tools</h3>
                 <div class="g-col offset_default flex-start">
                     <div class="one-half">
+                        <form id="form1" action="https://download.magplus.com/" class="magplus-download magform" method="get">
+                            <div class="ps-split">
+                                <select name="cs_version" id="cs_version" class="cs_version">
+                                    <!--<option value="">Select your Adobe InDesign version</option>-->
+                                    <option value="CC_2020" id="cs_version_CC_2020">CC 2020</option>
+                                    <option value="CC_2019" id="cs_version_CC_2019">CC 2019</option>
+                                    <option value="CC_2018" id="cs_version_CC_2018">CC 2018</option>
+                                    <!--<option value="CC_2017" id="cs_version_CC_2017">CC 2017</option>
+                                    <option value="CC_2015" id="cs_version_CC_2015">CC 2015</option>
+                                    <option value="CC_2014" id="cs_version_CC_2014">CC 2014</option>
+                                    <option value="CC" id="cs_version_CC">CC</option>-->
+                                    <option value="CS6" id="cs_version_CS6">CS6</option>
+                                    <!--<option value="CS5.5" id="cs_version_CS5_5">CS5.5</option>
+                                    <option value="CS5" id="cs_version_CS5">CS5</option>-->
+                                    
+                                </select>
+                                <div class="ver_indver"><p><span class="indver">Shown above is the latest InDesign version. To select a different version, choose from the drop-down menu. </span></p></div>
+                            </div>
+                            <div class="ps-split">
+                                <label class="ps-split-label" for="os_MacOS"><input name="os" tabindex="8" id="osMac" value="MacOS" type="radio">  Mac OS X</label>
+                                 <label for="os_Win"><input name="os" tabindex="9" id="osWin" value="Win" type="radio"> Windows</label>
+                            </div>
+                            <div>
+                                <input name="submit" id="download-designd-tools-submit-button" class="secondary-button download" tabindex="7" value="Download" type="submit">
+                            </div>
+                        </form>
+                        <!--
+                        <div style="display: none !important;">
+                            <form id="form2">
+                                <input id="text0" name="text0" type="hidden" value="<?php echo $data->user_id; ?>" /><br />
+                                <input id="text1" name="text1" type="hidden" value="<?php echo $data->name; ?>" /><br />
+                                <input name="text2" type="hidden" value="<?php echo $data->email; ?>"/><br />
+                                <input name="phone" type="hidden" value="<?php echo $data->phone; ?>"/><br />
+                                <input id="text3" name="text3" type="hidden" value="<?php echo $nowFormat; ?>"/><br />
+                                <input id="time31" name="text31" type="hidden" value="<?php echo $timeFormat; ?>"/><br />
+                                <input id="text5" name="text5" type="hidden" value=""/><br />
+                                <input id="text4" name="text4" type="hidden" value=""/><br />
+                                <input id="text6" name="text6" type="hidden" value="<?php echo $ipaddress; ?>"/><br />
+                            </form>
+                        </div>
+                        -->
+                        <!--
                         <div class="download-ver-select"> 
                             <div class="selected">
                                 <a href="javascript:void"><span> CC 2020</span></a>
@@ -27,7 +101,7 @@ get_header();
                                     <li><a href="javascript:void" id="ver18"> CC 2018</a></li>
                                 </ul>
                             </div>    
-                        </div>  
+                        </div>
                         <div class="version-block ver20">
                             
                             <p>Shown above is the latest InDesign version. To select a different version, choose from the drop-down menu.</p>
@@ -98,6 +172,7 @@ get_header();
                             </div>
                         </div>   
                         <p class="tmc-text">By downloading I am agreeing to the  <a href="?page_id=31545" class="link" title="mag+ Designd Terms & Conditions">mag+ Designd Terms & Conditions</a></p>
+                        -->
                     </div>
                     <div class="one-half">
                         <div class="tools-download-info-holder">
