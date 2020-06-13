c127be87797df11f40ebebccba05f3ae5089fb87
Arjun Sonawane <arjunsonawane@Arjuns-MacBook-Air.local>
2020-06-13 13:26:41 +0530

    Committing download page changes

--- wp-content/themes/twentynineteen/templates/download.php
+++ wp-content/themes/twentynineteen/templates/download.php
@@ -50,29 +50,51 @@ get_header();
                     <div class="one-half">
                         <form id="form1" action="https://download.magplus.com/" class="magplus-download magform" method="get">
                             <div class="ps-split">
-                                <select name="cs_version" id="cs_version" class="cs_version">
-                                    <!--<option value="">Select your Adobe InDesign version</option>-->
+                                <!-- <select name="cs_version" id="cs_version" class="cs_version">
+                                    <option value="">Select your Adobe InDesign version</option>
                                     <option value="CC_2020" id="cs_version_CC_2020">CC 2020</option>
                                     <option value="CC_2019" id="cs_version_CC_2019">CC 2019</option>
                                     <option value="CC_2018" id="cs_version_CC_2018">CC 2018</option>
-                                    <!--<option value="CC_2017" id="cs_version_CC_2017">CC 2017</option>
-                                    <option value="CC_2015" id="cs_version_CC_2015">CC 2015</option>
-                                    <option value="CC_2014" id="cs_version_CC_2014">CC 2014</option>
-                                    <option value="CC" id="cs_version_CC">CC</option>-->
-                                    <option value="CS6" id="cs_version_CS6">CS6</option>
-                                    <!--<option value="CS5.5" id="cs_version_CS5_5">CS5.5</option>
-                                    <option value="CS5" id="cs_version_CS5">CS5</option>-->
-                                    
-                                </select>
-                                <div class="ver_indver"><p><span class="indver">Shown above is the latest InDesign version. To select a different version, choose from the drop-down menu. </span></p></div>
+                                    <option value="CS6" id="cs_version_CS6">CS6</option>    
+                                </select> -->
+
+                                <div class="download-ver-select"> 
+                                    <div class="selected">
+                                        <a href="javascript:void"><span> CC 2020</span></a>
+                                    </div>
+                                    <div class="options">
+                                        <ul name="cs_version" id="cs_version" class="cs_version"> 
+                                            <li><a href="javascript:void" id="cs_version_CC_2020"> CC 2020</a></li>
+                                            <li><a href="javascript:void" id="cs_version_CC_2019" > CC 2019</a></li>
+                                            <li><a href="javascript:void" id="cs_version_CC_2018"> CC 2018</a></li>
+                                            <li><a href="javascript:void" id="cs_version_CS6"> CS6</a></li>
+                                        </ul>
+                                    </div>    
+                                </div>
+                                        
+                                <p style="margin-bottom: 15px;">Shown above is the latest InDesign version. To select a different version, choose from the drop-down menu. </p>
                             </div>
-                            <div class="ps-split">
-                                <label class="ps-split-label" for="os_MacOS"><input name="os" tabindex="8" id="osMac" value="MacOS" type="radio">  Mac OS X</label>
-                                 <label for="os_Win"><input name="os" tabindex="9" id="osWin" value="Win" type="radio"> Windows</label>
-                            </div>
-                            <div>
-                                <input name="submit" id="download-designd-tools-submit-button" class="secondary-button download" tabindex="7" value="Download" type="submit">
-                            </div>
+
+
+                            <div class="radio-button-block tmc-block ps-split">
+                                <label class="ps-split-label radio-container" for="osMac">Mac OS X
+                                    <input name="os" tabindex="8" id="osMac" value="MacOS" type="radio">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container" for="osWin">Windows
+                                    <input name="os" tabindex="9" id="osWin" value="Win" type="radio">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>    
+
+                            <div class="btn-block">
+                                <!-- <a href="" class="primary-btn" title="Download">Download
+                                    <span class="span1">Download</span>
+                                    <span class="span2">Download</span>
+                                    <span class="span3">Download</span>
+                                </a> -->
+                                <input name="submit" id="download-designd-tools-submit-button" class="primary-btn download" tabindex="7" value="Download" type="submit">
+                            </div>    
                         </form>
                         <!--
                         <div style="display: none !important;">
@@ -171,8 +193,9 @@ get_header();
                                 </a>
                             </div>
                         </div>   
-                        <p class="tmc-text">By downloading I am agreeing to the  <a href="?page_id=31545" class="link" title="mag+ Designd Terms & Conditions">mag+ Designd Terms & Conditions</a></p>
                         -->
+                        <p class="tmc-text">By downloading I am agreeing to the  <a href="?page_id=31545" class="link" title="mag+ Designd Terms & Conditions">mag+ Designd Terms & Conditions</a></p>
+                        
                     </div>
                     <div class="one-half">
                         <div class="tools-download-info-holder">
